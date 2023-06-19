b8ff5b0404aad4364b824123ea15c8048b5756d6
JuanjoChiliutti <95381544+JuanjoChiliutti@users.noreply.github.com>
2023-06-19 15:55:51 +0200

    styles 50%

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 
 import './App.css'
 
+
 function App() {
 
 
@@ -8,29 +9,38 @@ function App() {
     <>
       <div className='principal'>
         <div className='left-side-bar'></div>
-        <div className='main-form-v'>
-          <div className='cardholder'>
-            <label htmlFor="">CARDHOLDER NAME</label>
-            <input type="text" placeholder='e.g. Jane Appleseed'/>
+        <div className='main-form'>
+          <div className='main-form-v'>
+            <div className='cardholder'>
+              <label htmlFor="" className='etiqueta'>CARDHOLDER NAME</label>
+              <input type="text" placeholder='e.g. Jane Appleseed' className='caja'/>
+            </div>
+            <div className='cardnumber'>
+              <label htmlFor="" className='etiqueta'>CARD NUMBER</label>
+              <input type="number" placeholder='e.g. 1234 5678 9123 0000' className='caja'/>
+            </div>
           </div>
-          <div className='cardnumber'>
-            <label htmlFor="">CARD NUMBER</label>
-            <input type="number" placeholder='e.g. 1234 5678 9123 0000' />
+          <div className='main-form-h'>
+            <div className='exp'>
+              <label htmlFor="" className='etiqueta'>EXP. DATE (MM/YY)</label>
+              <div className='expdate'>
+                <input type="number" placeholder='MM' className='caja mesdia'/>
+                <input type="number" placeholder='YY' className='caja mesdia'/>
+              </div>
+            </div>
+            <div className='cvc'>
+              <label htmlFor="" className='etiqueta'>CVC</label>
+              <input type="number" placeholder='e.g. 123' className='caja'/>
+            </div>
           </div>
+            <button className='btn-confirm'>Confirm</button>
         </div>
-        <div className='main-form-h'>
-          <div className='exp'>
-            <label htmlFor="">EXP. DATE (MM/YY)</label>
-            <input type="number" placeholder='MM'/>
-            <input type="number" placeholder='YY'/>
-          </div>
-          <div className='cvc'>
-            <label htmlFor="">CVC</label>
-            <input type="number" placeholder='e.g. 123'/>
-          </div>
+        <div className='card'>
+            <div className='card-front'></div>
+            <div className='card-back'></div>
         </div>
       </div>
-     
+      
     </>
   )
 }
