441f49b21dbd38ee4129e2a5cf9a212c1bd7871b
JuanjoChiliutti <95381544+JuanjoChiliutti@users.noreply.github.com>
2023-06-20 16:05:03 +0200

    input states

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
 
 import './App.css'
+import { useState } from 'react'
 
 
 function App() {
-
+  const [input, setInput] = useState({
+    nombre: 'JANE APPLESSED',
+    numero: '0000 0000 0000 0000',
+    mes: '00',
+    ano: '00',
+    cvc: '000'
+  })
+  
+  function handleChange(event) {
+    event.preventDefault();
+    setInput(prevState => ({
+      ...prevState,
+      [event.target.name]: event.target.value.toUpperCase()
+    }));
+  }
 
   return (
     <>
@@ -13,34 +28,47 @@ function App() {
           <div className='main-form-v'>
             <div className='cardholder'>
               <label htmlFor="" className='etiqueta'>CARDHOLDER NAME</label>
-              <input type="text" placeholder='e.g. Jane Appleseed' className='caja'/>
+              <input type="text" name='nombre' onChange={handleChange} placeholder='e.g. Jane Appleseed' className='caja' />
             </div>
             <div className='cardnumber'>
               <label htmlFor="" className='etiqueta'>CARD NUMBER</label>
-              <input type="number" placeholder='e.g. 1234 5678 9123 0000' className='caja'/>
+              <input type="number" onChange={handleChange} name='numero' placeholder='e.g. 1234 5678 9123 0000' className='caja' />
             </div>
           </div>
           <div className='main-form-h'>
             <div className='exp'>
               <label htmlFor="" className='etiqueta'>EXP. DATE (MM/YY)</label>
               <div className='expdate'>
-                <input type="number" placeholder='MM' className='caja mesdia'/>
-                <input type="number" placeholder='YY' className='caja mesdia'/>
+                <input type="number" name='mes' onChange={handleChange} placeholder='MM' className='caja mesdia' />
+                <input type="number" name='ano' onChange={handleChange} placeholder='YY' className='caja mesdia' />
               </div>
             </div>
             <div className='cvc'>
               <label htmlFor="" className='etiqueta'>CVC</label>
-              <input type="number" placeholder='e.g. 123' className='caja'/>
+              <input type="number" onChange={handleChange} name='cvc' placeholder='e.g. 123' className='caja' />
             </div>
           </div>
-            <button className='btn-confirm'>Confirm</button>
+          <button className='btn-confirm'>Confirm</button>
         </div>
-        <div className='card'>
-            <div className='card-front'></div>
-            <div className='card-back'></div>
-        </div>
+        
+          <div className='card-front'>
+            <div className='contenedor-imagen'>
+            </div>
+            <div className='contenedor-numero'>
+              <p className='nroTarjeta'>{input.numero}</p>
+            </div>
+            <div className='enlinea'>
+              <span className='nombre datos'>{input.nombre}</span><span className='exp datos'>{input.mes}/{input.ano}</span>
+            </div>
+          </div>
+          <div className='card-back'>
+            <div className='nrocvc datos'>
+              <p>{input.cvc}</p>
+            </div>
+          </div>
+        
       </div>
-      
+
     </>
   )
 }
